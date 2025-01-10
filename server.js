331d5966624eef4acfd18db30d2a3afb7eff7a7f
CodePioneer2 <166389859+CodePioneer2@users.noreply.github.com>
2025-01-11 01:05:06 +0200

    added Express and styling

--- server.js
+++ server.js
@@ -1,41 +1,32 @@
-import 'dotenv/config' // Import dotenv
-import http from 'http'; // Modern ESM imports
-import fs from 'fs'; // Older way is to write => const fs = require("fs")
+import 'dotenv/config';
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 
+const app = express();
+const port = process.env.port || 5000;
 
-const getHTML = (filepath, res) => {
-  // Reading filepath from server
-  fs.readFile(filepath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(err);
-      res.statusCode = 500;
-      res.end('Error reading the file');
-      return;
-    }
-    // File reading succesful sending it back to client
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data);
-  });
-};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-const server = http.createServer((req, res) => {
-  // Homepage
-  if (req.url === '/') {
-    getHTML('./index.html', res);
-  } else if (req.url === '/about') {
-    getHTML('./about.html', res);
-  } else if (req.url === '/contact-me') {
-    getHTML('./contact-me.html', res);
-  } else {
-    getHTML('./404.html', res); // All other routes leading 404
-  }
+app.use(express.static(__dirname)); //include css
+
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, 'index.html'));
 });
 
-// Default port 5000 if no env.PORT
-const PORT = process.env.PORT || 5000;
+app.get('/about', (req, res) => {
+  res.sendFile(join(__dirname, 'about.html'));
+});
 
-// Summon server on port 5000
-server.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+app.get('/contact-me', (req, res) => {
+  res.sendFile(join(__dirname, 'contact-me.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, '404.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
